38352a524e0c9233e6ca800a49b75ecf7b3bc39d
Marwinka <marwinka@inbox.ru>
2026-04-30 20:37:25 +0300

    bruuh

---
+++ build/CMakeFiles/BurnHopeEngine.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/glad.c" "CMakeFiles/BurnHopeEngine.dir/Src/Source/glad.c.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/glad.c.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Camera.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Camera.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Camera.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/EBO.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/EBO.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/EBO.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Main.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Main.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Main.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Material.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Material.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Material.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Mesh.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Mesh.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Mesh.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Model.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Model.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Model.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/ModelImporter.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/ModelImporter.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/ModelImporter.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/PhysicsEngine.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/PhysicsEngine.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/PhysicsEngine.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/PostProcessingShader.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/PostProcessingShader.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/PostProcessingShader.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Render.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Render.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Render.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/Texture.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Texture.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/Texture.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/TextureImporter.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/TextureImporter.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/TextureImporter.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/TextureStreamer.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/TextureStreamer.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/TextureStreamer.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/VAO.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/VAO.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/VAO.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/VBO.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/VBO.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/VBO.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/shaderClass.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/shaderClass.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/shaderClass.cpp.o.d"
+  "/home/marwinka/Музыка/BurnhopeEngine/Src/Source/stb.cpp" "CMakeFiles/BurnHopeEngine.dir/Src/Source/stb.cpp.o" "gcc" "CMakeFiles/BurnHopeEngine.dir/Src/Source/stb.cpp.o.d"
+  "" "BurnHopeEngine" "gcc" "CMakeFiles/BurnHopeEngine.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BurnHopeEngine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BurnHopeEngine.
+# This may be replaced when dependencies are built.
